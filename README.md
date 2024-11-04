c92e93088fa396a482edbe2b09fc3420e37a4922
Gabo <ghasquio@unemi.edu.ec>
2024-11-04 10:43:56 -0500

    crud añadidos y cambios en la estructura del proyecto

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 
+# App de Gestión Médica
+
+## Descripción
+
+La **App de Gestión Médica** es una aplicación diseñada para facilitar la administración de pacientes, doctores, citas médicas y otros aspectos relacionados con la atención médica en una clínica. La aplicación permite registrar y gestionar información detallada sobre pacientes, doctores, medicamentos, diagnósticos, exámenes médicos y más.
+
+## Características
+
+- **Gestión de Pacientes**: Registro y administración de información personal y médica de los pacientes.
+- **Gestión de Doctores**: Registro y administración de información profesional y personal de los doctores.
+- **Citas Médicas**: Programación y seguimiento de citas médicas.
+- **Exámenes Médicos**: Registro y administración de exámenes médicos solicitados.
+- **Servicios Adicionales**: Gestión de servicios adicionales ofrecidos durante la atención médica.
+- **Costos de Atención**: Registro y administración de los costos asociados a la atención médica.
+
+## Estructura del Proyecto
+
+El proyecto está organizado en varios módulos, cada uno con responsabilidades específicas:
+
+- **aplication/core**: Contiene los modelos, formularios y vistas principales de la aplicación.
+- **aplication/attention**: Contiene modelos y migraciones relacionados con la atención médica, incluyendo citas y costos.
+- **doctor**: Contiene scripts y configuraciones específicas para la gestión de doctores.
+- **template/core**: Contiene las plantillas HTML para la interfaz de usuario, organizadas por funcionalidad.
+
+## Instalación
+
+1. Clona el repositorio:
+    ```sh
+    git clone https://github.com/tu_usuario/app_doctor.git
+    ```
+2. Navega al directorio del proyecto:
+    ```sh
+    cd app_doctor
+    ```
+3. Instala las dependencias:
+    ```sh
+    pip install -r requirements.txt
+    ```
+4. Realiza las migraciones de la base de datos:
+    ```sh
+    python manage.py migrate
+    ```
+5. Inicia el servidor de desarrollo:
+    ```sh
+    python manage.py runserver
+    ```
+
+## Uso
+
+- Accede a la aplicación en tu navegador web en `http://localhost:8000`.
+- Utiliza el menú de navegación para acceder a las diferentes secciones de la aplicación, como la gestión de pacientes, doctores y citas médicas.
+
+
+## Licencia
+
+Este proyecto está licenciado bajo la Licencia MIT. Consulta el archivo `LICENSE` para más detalles.
+
+## Contacto
+
+Para más información, puedes contactar al equipo de desarrollo en [correo@ejemplo.com](mailto:correo@ejemplo.com).
---
+++ aplication/attention/forms/Attention_hours.py
@@ -0,0 +1,96 @@
+from django.forms import ModelForm, ValidationError
+from django import forms
+from aplication.attention.models import HorarioAtencion
+
+class AttentionHoursForm(ModelForm):
+    class Meta:
+        model = HorarioAtencion
+        fields = ['dia_semana', 'hora_inicio', 'hora_fin', 'Intervalo_desde', 'Intervalo_hasta', 'activo']
+        
+        error_messages = {
+            'dia_semana': {
+                'required': "El campo día de la semana es requerido"
+            },
+            'hora_inicio': {
+                'required': "El campo hora de inicio es requerido"
+            },
+            'hora_fin': {
+                'required': "El campo hora de fin es requerido"
+            },
+            'Intervalo_desde': {
+                'required': "El campo intervalo desde es requerido"
+            },
+            'Intervalo_hasta': {
+                'required': "El campo intervalo hasta es requerido"
+            },
+            'activo': {
+                'required': "El campo activo es requerido"
+            }
+        }
+        
+        widgets = {
+            'dia_semana': forms.Select(
+                attrs={
+                    "id": "id_dia_semana",
+                    "class": "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-12 dark:bg-principal dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light",
+                }
+            ),
+            'hora_inicio': forms.TimeInput(
+                attrs={
+                    "placeholder": "Ingrese hora de inicio",
+                    "id": "id_hora_inicio",
+                    "class": "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-12 dark:bg-principal dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light",
+                }
+            ),
+            'hora_fin': forms.TimeInput(
+                attrs={
+                    "placeholder": "Ingrese hora de fin",
+                    "id": "id_hora_fin",
+                    "class": "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-12 dark:bg-principal dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light",
+                }
+            ),
+            'Intervalo_desde': forms.TimeInput(
+                attrs={
+                    "placeholder": "Ingrese intervalo desde",
+                    "id": "id_intervalo_desde",
+                    "class": "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-12 dark:bg-principal dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light",
+                }
+            ),
+            'Intervalo_hasta': forms.TimeInput(
+                attrs={
+                    "placeholder": "Ingrese intervalo hasta",
+                    "id": "id_intervalo_hasta",
+                    "class": "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pr-12 dark:bg-principal dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light",
+                }
+            ),
+            'activo': forms.CheckboxInput(
+                attrs={
+                    "id": "id_activo",
+                    "class": "shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 block dark:bg-principal dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light",
+                }
+            ),
+        }
+
+    def clean_hora_inicio(self):
+        hora_inicio = self.cleaned_data.get('hora_inicio')
+        if not hora_inicio:
+            raise ValidationError("El campo hora de inicio es requerido")
+        return hora_inicio
+
+    def clean_hora_fin(self):
+        hora_fin = self.cleaned_data.get('hora_fin')
+        if not hora_fin:
+            raise ValidationError("El campo hora de fin es requerido")
+        return hora_fin
+
+    def clean_Intervalo_desde(self):
+        Intervalo_desde = self.cleaned_data.get('Intervalo_desde')
+        if not Intervalo_desde:
+            raise ValidationError("El campo intervalo desde es requerido")
+        return Intervalo_desde
+
+    def clean_Intervalo_hasta(self):
+        Intervalo_hasta = self.cleaned_data.get('Intervalo_hasta')
+        if not Intervalo_hasta:
+            raise ValidationError("El campo intervalo hasta es requerido")
+        return Intervalo_hasta
